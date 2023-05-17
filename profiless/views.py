35a4244fc80c979d4f8e101ae80c3a2ca4dea331
email2ify <>
2023-05-17 23:01:21 +0000

    Add login,logout views,isowner permission and profile

--- profiless/views.py
+++ profiless/views.py
@@ -4,21 +4,24 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Profile
 from .serializers import ProfileSerializer
+from django_app.permissions import IsOwnerOrReadOnly
 
 
 
 class ProfileList(APIView):
     def get(self, request):
         profiless = Profile.objects.all()
-        serializer = ProfileSerializer(profiless, many=True)
+        serializer = ProfileSerializer(profiless, many=True, context={'request': request})
         return Response(serializer.data)
 
 
 class ProfileDetail(APIView):
     serializer_class = ProfileSerializer
+    permission_classes = [IsOwnerOrReadOnly]
     def get_object(self, pk):
         try:
             profile = Profile.objects.get(pk=pk)
+            self.check_object_permissions(self.request, profile)
             return profile
         except Profile.DoesNotExist:
             raise Http404
@@ -30,7 +33,7 @@ class ProfileDetail(APIView):
 
     def put(self, request, pk):
         profile = self.get_object(pk)
-        serializer = ProfileSerializer(profile, data=request.data)
+        serializer = ProfileSerializer(profile, data=request.data, context={'request': request})
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
